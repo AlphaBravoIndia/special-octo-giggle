2900fd6c4ed725dbcf92eb55c18dd63f7f01c8ea
Abi Tyas Tunggal <abi@Scarletts-MacBook-Air.local>
2023-01-12 15:31:19 +1100

    update url

--- middleware.ts
+++ middleware.ts
@@ -24,11 +24,6 @@ export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
-  // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "curious.ventures" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.curious.ventures");
-  }
-
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
@@ -37,7 +32,7 @@ export default async function middleware(req: NextRequest) {
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
           .replace(`.curious.ventures`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.special-octo-giggle.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   // rewrites for app pages
@@ -56,7 +51,7 @@ export default async function middleware(req: NextRequest) {
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
+  if (hostname === "localhost:3000" || hostname === "special-octo-giggle.vercel.app") {
     return NextResponse.rewrite(new URL(`/home/${path}`, req.url));
   }
 
